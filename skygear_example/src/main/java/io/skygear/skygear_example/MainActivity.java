afb46b6789b483ad367748c459055977a399d160
Ben Lei <benlei@oursky.com>
2016-06-03 17:46:08 +0800

    Add skygear to exmaple app
    Ref. #2

--- skygear_example/src/main/java/io/skygear/skygear_example/MainActivity.java
+++ skygear_example/src/main/java/io/skygear/skygear_example/MainActivity.java
@@ -1,18 +1,35 @@
 package io.skygear.skygear_example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import io.skygear.skygear.Configuration;
 import io.skygear.skygear.Container;
 
 public class MainActivity extends AppCompatActivity {
     private Container skygear;
+    private TextView endpointDisplay;
+    private TextView apiKeyDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        skygear = new Container();
+        this.endpointDisplay = (TextView) findViewById(R.id.endpoint_display);
+        this.apiKeyDisplay = (TextView) findViewById(R.id.api_key_display);
+
+        this.skygear = Container.defaultContainer();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Configuration skygearConfig = this.skygear.getConfig();
+
+        this.endpointDisplay.setText(String.format("Endpoint: %s", skygearConfig.endpoint));
+        this.apiKeyDisplay.setText(String.format("API Key: %s", skygearConfig.apiKey));
     }
 }
---
+++ skygear_example/src/main/java/io/skygear/skygear_example/MyApplication.java
@@ -0,0 +1,18 @@
+package io.skygear.skygear_example;
+
+import io.skygear.skygear.SkygearApplication;
+
+/**
+ * Created by benlei on 3/6/2016.
+ */
+public class MyApplication extends SkygearApplication {
+    @Override
+    public String getSkygearEndpoint() {
+        return "http://skygear.dev/";
+    }
+
+    @Override
+    public String getApiKey() {
+        return "dev";
+    }
+}
