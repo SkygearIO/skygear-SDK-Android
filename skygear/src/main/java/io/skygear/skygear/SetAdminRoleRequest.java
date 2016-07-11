3987de29ba0ca4dd91784949a39013c6a5e038e0
Ben Lei <benlei@oursky.com>
2016-07-11 11:54:11 +0800

    Enable to set admin role
    Ref. #14

---
+++ skygear/src/main/java/io/skygear/skygear/SetAdminRoleRequest.java
@@ -0,0 +1,35 @@
+package io.skygear.skygear;
+
+import java.util.HashMap;
+
+/**
+ * The Skygear Admin Role Setup Request.
+ */
+public class SetAdminRoleRequest extends Request {
+    /**
+     * Instantiates a new Set Admin Role Request.
+     *
+     * @param roles the roles array
+     */
+    public SetAdminRoleRequest(Role[] roles) {
+        super("role:admin");
+
+        this.data = new HashMap<>();
+
+        String[] roleNames = new String[roles.length];
+        for (int idx = 0; idx < roles.length; idx++) {
+            roleNames[idx] = roles[idx].getName();
+        }
+
+        this.data.put("roles", roleNames);
+    }
+
+    /**
+     * Instantiates a new Set Admin Role Request for a Role.
+     *
+     * @param role the role
+     */
+    public SetAdminRoleRequest(Role role) {
+        this(new Role[]{ role });
+    }
+}
