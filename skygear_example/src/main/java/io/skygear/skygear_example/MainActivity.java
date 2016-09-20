0a81d3ef3feb84bc0ec95307df058621417fb76a
Ben Lei <benlei@oursky.com>
2016-09-20 16:59:29 +0800

    Update sample project to support asset
    Ref. #43

--- skygear_example/src/main/java/io/skygear/skygear_example/MainActivity.java
+++ skygear_example/src/main/java/io/skygear/skygear_example/MainActivity.java
@@ -1,10 +1,8 @@
 package io.skygear.skygear_example;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
--- skygear_example/src/main/java/io/skygear/skygear_example/RecordCreateActivity.java
+++ skygear_example/src/main/java/io/skygear/skygear_example/RecordCreateActivity.java
@@ -1,33 +1,55 @@
 package io.skygear.skygear_example;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONException;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Map;
 
+import io.skygear.skygear.Asset;
+import io.skygear.skygear.AssetPostRequest;
 import io.skygear.skygear.Container;
 import io.skygear.skygear.Record;
 import io.skygear.skygear.RecordDeleteResponseHandler;
 import io.skygear.skygear.RecordSaveResponseHandler;
 
 public class RecordCreateActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQ = 12345;
+
     private EditText[] recordKeyFields;
     private EditText[] recordValueFields;
 
-    private Container skygear;
-    private Record record;
+    private EditText recordAssetKeyField;
+    private ImageView recordAssetImageView;
+    private Button recordAssetButton;
+
     private TextView display;
     private Button deleteButton;
+
+    private Container skygear;
+    private Record record;
+    private Asset recordAsset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +68,15 @@ public class RecordCreateActivity extends AppCompatActivity {
                 (EditText) findViewById(R.id.record_value2)
         };
 
+        this.recordAssetKeyField = (EditText) findViewById(R.id.record_asset_key);
+        this.recordAssetImageView = (ImageView) findViewById(R.id.record_asset_image_view);
+        this.recordAssetButton = (Button) findViewById(R.id.record_asset_button);
+
         this.deleteButton = (Button) findViewById(R.id.delete_button);
         this.display = (TextView) findViewById(R.id.record_display);
 
         this.updateRecordDisplay();
+        this.updateAssetViews();
     }
 
     private void updateRecordDisplay() {
@@ -74,6 +101,35 @@ public class RecordCreateActivity extends AppCompatActivity {
         this.display.setText(displayText);
     }
 
+    private void updateAssetViews() {
+        if (this.recordAsset == null) {
+            this.recordAssetImageView.setImageDrawable(null);
+            this.recordAssetButton.setText(R.string.set_image);
+            this.recordAssetButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    RecordCreateActivity.this.doPickImage(v);
+                }
+            });
+
+            return;
+        }
+
+        Bitmap bitmap = BitmapFactory.decodeByteArray(
+                this.recordAsset.getData(),
+                0,
+                (int) this.recordAsset.getSize()
+        );
+        this.recordAssetImageView.setImageDrawable(new BitmapDrawable(null, bitmap));
+        this.recordAssetButton.setText(R.string.remove_image);
+        this.recordAssetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecordCreateActivity.this.doRemoveImage(v);
+            }
+        });
+    }
+
     private void dismissKeyboard() {
         View currentFocus = this.getCurrentFocus();
         if (currentFocus != null) {
@@ -94,6 +150,11 @@ public class RecordCreateActivity extends AppCompatActivity {
             if (keyString.length() > 0) {
                 newRecord.set(keyString, valueString);
             }
+        }
+
+        String recordAssetKey = this.recordAssetKeyField.getText().toString().trim();
+        if (recordAssetKey.length() > 0 && this.recordAsset != null) {
+            newRecord.set(recordAssetKey, this.recordAsset);
         }
 
         final AlertDialog successDialog = new AlertDialog.Builder(this)
@@ -188,4 +249,97 @@ public class RecordCreateActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void doRemoveImage(View view) {
+        this.recordAsset = null;
+        this.updateAssetViews();
+    }
+
+    public void doPickImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case PICK_IMAGE_REQ:
+                if (resultCode == RESULT_OK) {
+                    this.handleImagePick(data.getData());
+
+
+                }
+        }
+    }
+
+    private void handleImagePick(Uri uri) {
+        Log.i("RecordCreateActivity", "handleImagePick: Got URI: " + uri);
+        ContentResolver contentResolver = getContentResolver();
+
+        try {
+            final InputStream inputStream = contentResolver.openInputStream(uri);
+            final String mimeType = contentResolver.getType(uri);
+            Log.i("RecordCreateActivity", "handleImagePick: Got MIME-Type: " + mimeType);
+
+            final ProgressDialog loading = new ProgressDialog(this);
+            loading.setTitle("Loading");
+            loading.setMessage("Decoding image...");
+            loading.setCancelable(false);
+            loading.show();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("RecordCreateActivity", "handleImagePick: Start decoding the image");
+
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+
+                    final byte[] bytes = byteArrayOutputStream.toByteArray();
+                    Log.i("RecordCreateActivity", "handleImagePick: Finish decoding, size: " + bytes.length);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            loading.dismiss();
+                            RecordCreateActivity.this.handleImageUpload(bytes, mimeType);
+                        }
+                    });
+                }
+            }).start();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleImageUpload(byte[] data, String mimeType) {
+        final ProgressDialog loading = new ProgressDialog(this);
+        loading.setTitle("Loading");
+        loading.setMessage("Uploading image...");
+        loading.setCancelable(false);
+        loading.show();
+
+        Asset asset = new Asset("Record-Image", mimeType, data);
+        this.skygear.uploadAsset(asset, new AssetPostRequest.ResponseHandler() {
+            @Override
+            public void onPostSuccess(Asset asset, String response) {
+                Log.i("RecordCreateActivity", "handleImageUpload: successfully uploaded to " + asset.getUrl());
+                RecordCreateActivity.this.recordAsset = asset;
+                RecordCreateActivity.this.updateAssetViews();
+                loading.dismiss();
+            }
+
+            @Override
+            public void onPostFail(Asset asset, String reason) {
+                Log.i("RecordCreateActivity", "handleImageUpload: fail - " + reason);
+                loading.dismiss();
+            }
+        });
+    }
+
 }
