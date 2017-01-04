68956e93132bd902505ca2cd356d470df1cbba1f
Ben Lei <benlei@oursky.com>
2017-01-04 18:57:54 +0800

    Provide package name when register device

--- skygear/src/main/java/io/skygear/skygear/RegisterDeviceRequest.java
+++ skygear/src/main/java/io/skygear/skygear/RegisterDeviceRequest.java
@@ -20,9 +20,12 @@ public class RegisterDeviceRequest extends Request {
      * Instantiates a new Skygear Register Device Request with
      * Device ID and Device Token
      *
+     * @deprecated use {@link #RegisterDeviceRequest(String, String, String)} instead.
+     *
      * @param deviceId    the device id
      * @param deviceToken the device token
      */
+    @Deprecated
     public RegisterDeviceRequest(String deviceId, String deviceToken) {
         this();
 
@@ -32,6 +35,30 @@ public class RegisterDeviceRequest extends Request {
 
         if (deviceToken != null) {
             this.data.put("device_token", deviceToken);
+        }
+    }
+
+    /**
+     * Instantiates a new Skygear Register Device Request with
+     * Device ID, Device Token and Package Name
+     *
+     * @param deviceId    the device id
+     * @param deviceToken the device token
+     * @param packageName the package name
+     */
+    public RegisterDeviceRequest(String deviceId, String deviceToken, String packageName) {
+        this();
+
+        if (deviceId != null) {
+            this.data.put("id", deviceId);
+        }
+
+        if (deviceToken != null) {
+            this.data.put("device_token", deviceToken);
+        }
+
+        if (packageName != null) {
+            this.data.put("topic", packageName);
         }
     }
 }
