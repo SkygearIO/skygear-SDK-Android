d32357b92e1c7f0e3a4f207a2dd4a6b6fab608b9
Fung <funglam@astri.org>
2017-02-15 10:42:50 +0800

    Fix - runtime permission and gallary picker

--- skygear_example/src/main/java/io/skygear/skygear_example/RecordCreateActivity.java
+++ skygear_example/src/main/java/io/skygear/skygear_example/RecordCreateActivity.java
@@ -46,16 +46,19 @@ import io.skygear.skygear.Error;
 import io.skygear.skygear.Record;
 import io.skygear.skygear.RecordDeleteResponseHandler;
 import io.skygear.skygear.RecordSaveResponseHandler;
+import io.skygear.skygear_example.io.skygear.util.PermissionUtils;
 
 public class RecordCreateActivity
         extends AppCompatActivity
         implements
-            GoogleApiClient.ConnectionCallbacks,
-            GoogleApiClient.OnConnectionFailedListener
-{
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = RecordCreateActivity.class.getSimpleName();
     private static final int PICK_IMAGE_REQ = 12345;
     private static final int LOCATION_PERMISSION_REQ_CODE = 12346;
+
+    private static final int GALLERY_PERMISSIONS_REQUEST = 0;
+//    private static final int GALLERY_IMAGE_REQUEST = 1;
 
     private EditText[] recordKeyFields;
     private EditText[] recordValueFields;
@@ -84,12 +87,12 @@ public class RecordCreateActivity
 
         this.skygear = Container.defaultContainer(this);
 
-        this.recordKeyFields = new EditText[] {
+        this.recordKeyFields = new EditText[]{
                 (EditText) findViewById(R.id.record_key1),
                 (EditText) findViewById(R.id.record_key2)
         };
 
-        this.recordValueFields = new EditText[] {
+        this.recordValueFields = new EditText[]{
                 (EditText) findViewById(R.id.record_value1),
                 (EditText) findViewById(R.id.record_value2)
         };
@@ -124,7 +127,7 @@ public class RecordCreateActivity
 
             ActivityCompat.requestPermissions(
                     this,
-                    new String[]{ Manifest.permission.ACCESS_FINE_LOCATION },
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     LOCATION_PERMISSION_REQ_CODE
             );
         } else {
@@ -217,7 +220,8 @@ public class RecordCreateActivity
             this.recordAssetButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    RecordCreateActivity.this.doPickImage(v);
+//                    RecordCreateActivity.this.doPickImage(v);
+                    RecordCreateActivity.this.startGalleryChooser();
                 }
             });
 
@@ -281,7 +285,7 @@ public class RecordCreateActivity
                 .setMessage("")
                 .create();
 
-        skygear.getPublicDatabase().save(newRecord, new RecordSaveResponseHandler(){
+        skygear.getPublicDatabase().save(newRecord, new RecordSaveResponseHandler() {
             @Override
             public void onSaveSuccess(Record[] records) {
                 RecordCreateActivity.this.record = records[0];
@@ -364,11 +368,22 @@ public class RecordCreateActivity
         });
     }
 
+    public void startGalleryChooser() {
+        if (PermissionUtils.requestPermission(this, GALLERY_PERMISSIONS_REQUEST, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select a photo"),
+                    PICK_IMAGE_REQ);
+        }
+    }
+
     public void doRemoveImage(View view) {
         this.recordAsset = null;
         this.updateAssetViews();
     }
 
+    @Deprecated
     public void doPickImage(View view) {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
