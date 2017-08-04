750af21f02a58990661214dfc01b88d5a92fa720
Kwok-kuen Cheung <me@cheungpat.com>
2017-08-04 10:26:23 +0800

    Upload doc prefixed with version to s3 bucket for CI
    Examples:
    
    * https://docs.skygear.io/android/reference/v1.1.0/
    * https://docs.skygear.io/android/reference/latest/
    * https://docs.skygear.io/android/reference/canary/

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,28 @@
 #!/bin/bash -e
 
-if [[ -z "$BINTRAY_USER" ]]; then
-  echo >&2 "Error: \$BINTRAY_USER is not set"
-  exit 1
+if [ -n "$TRAVIS_TAG" ]; then
+
+  if [[ -z "$BINTRAY_USER" ]]; then
+    echo >&2 "Error: \$BINTRAY_USER is not set"
+    exit 1
+  fi
+
+  if [[ -z "$BINTRAY_API_KEY" ]]; then
+    echo >&2 "Error: \$BINTRAY_API_KEY is not set"
+    exit 1
+  fi
+
+  ./gradlew :skygear:bintrayUpload
 fi
 
-if [[ -z "$BINTRAY_API_KEY" ]]; then
-  echo >&2 "Error: \$BINTRAY_API_KEY is not set"
-  exit 1
-fi
+# Update docs.skygear.io
 
-./gradlew :skygear:bintrayUpload
+if [ -n "$TRAVIS_TAG" ]; then
+    if [ "$TRAVIS_TAG" -eq "latest" ]; then
+        make doc-deploy VERSION=latest
+    else
+        make doc-deploy VERSION="v$TRAVIS_TAG"
+    fi
+else
+    make doc-deploy VERSION="${TRAVIS_BRANCH/master/canary}"
+fi
