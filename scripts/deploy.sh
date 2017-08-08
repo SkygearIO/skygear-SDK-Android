e60b99e250822c7e66cf8f05dee993b681228e84
Kwok-kuen Cheung <cheungpat@oursky.com>
2017-08-08 17:34:47 +0800

    Do not upload to bintray when tag is latest

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,25 @@
 #!/bin/bash -e
 
 if [ -n "$TRAVIS_TAG" ]; then
+  if [ "$TRAVIS_TAG" != "latest" ]; then
+    if [[ -z "$BINTRAY_USER" ]]; then
+      echo >&2 "Error: \$BINTRAY_USER is not set"
+      exit 1
+    fi
 
-  if [[ -z "$BINTRAY_USER" ]]; then
-    echo >&2 "Error: \$BINTRAY_USER is not set"
-    exit 1
+    if [[ -z "$BINTRAY_API_KEY" ]]; then
+      echo >&2 "Error: \$BINTRAY_API_KEY is not set"
+      exit 1
+    fi
+
+    ./gradlew :skygear:bintrayUpload
   fi
-
-  if [[ -z "$BINTRAY_API_KEY" ]]; then
-    echo >&2 "Error: \$BINTRAY_API_KEY is not set"
-    exit 1
-  fi
-
-  ./gradlew :skygear:bintrayUpload
 fi
 
 # Update docs.skygear.io
 
 if [ -n "$TRAVIS_TAG" ]; then
-    if [ "$TRAVIS_TAG" -eq "latest" ]; then
+    if [ "$TRAVIS_TAG" == "latest" ]; then
         make doc-deploy VERSION=latest
     else
         make doc-deploy VERSION="v$TRAVIS_TAG"
