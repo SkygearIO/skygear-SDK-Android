5d6f6d8fab8aeb654215194e6d66319acfea92e7
Ben Lei <benlei@oursky.com>
2018-08-23 18:41:35 +0800

    Ensure an empty line between versions in CHANGELOG

--- scripts/release-commit.sh
+++ scripts/release-commit.sh
@@ -19,7 +19,7 @@ else
 fi
 
 github-release release -u skygeario -r skygear-SDK-Android --draft --tag $SKYGEAR_VERSION --name "$SKYGEAR_VERSION" --description "`cat new-release`"
-cat CHANGELOG.md >> new-release && mv new-release CHANGELOG.md
+echo "" >> new-release && cat CHANGELOG.md >> new-release && mv new-release CHANGELOG.md
 make update-version VERSION=$SKYGEAR_VERSION
 git add CHANGELOG.md skygear/build.gradle
 git commit -m "Update CHANGELOG for $SKYGEAR_VERSION"
